912025088528680ead4f6128642b5a5123d624f8
dku124 <duongkhanhuyen124@gmail.com>
2024-03-29 17:33:24 +0700

    update

---
+++ js/main.js
@@ -0,0 +1,32 @@
+var swiper = new Swiper(".stepList", {
+  slidesPerView: 3,
+  spaceBetween: 20,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+var swiper2 = new Swiper(".customerSlider", {
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+var swiper3 = new Swiper(".blogSlider", {
+  slidesPerView: 3,
+  spaceBetween: 20,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+// var swiper3 = new Swiper(".blogSlider", {
+//   slidesPerView: 3,
+//   spaceBetween: 20,
+//   pagination: {
+//     el: ".swiper-pagination",
+//     clickable: true,
+//   },
+// });
