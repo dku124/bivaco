1984bc6c8af61907973282ed3e2086643551fc7a
dku124 <duongkhanhuyen124@gmail.com>
2024-03-30 12:01:12 +0700

    update 3

--- js/main.js
+++ js/main.js
@@ -22,11 +22,34 @@ var swiper3 = new Swiper(".blogSlider", {
   },
 });
 
-// var swiper3 = new Swiper(".blogSlider", {
-//   slidesPerView: 3,
-//   spaceBetween: 20,
-//   pagination: {
-//     el: ".swiper-pagination",
-//     clickable: true,
-//   },
-// });
+$(document).ready(function(){
+  $(window).scroll(function(){
+  	var scroll = $(window).scrollTop();
+	  if (scroll > 50) {
+	    $(".header").addClass('active');
+	  }
+
+	  else{
+		  $(".header").removeClass('active');  	
+	  }
+  })
+})
+
+// cursor
+
+var cursor = document.querySelector('.cursor');
+var cursorinner = document.querySelector('.cursor2');
+
+document.addEventListener('mousemove', function(e){
+  var x = e.clientX;
+  var y = e.clientY;
+  cursor.style.transform = `translate3d(calc(${e.clientX}px - 50%), calc(${e.clientY}px - 50%), 0)`
+});
+
+document.addEventListener('mousemove', function(e){
+  var x = e.clientX;
+  var y = e.clientY;
+  cursorinner.style.left = x + 'px';
+  cursorinner.style.top = y + 'px';
+});
+
